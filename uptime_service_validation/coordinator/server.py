2056cf03db8cd1ecf0e29691f08dcd4f204bb799
Simonas Narbutas <simonas@narbuto.lt>
2024-03-04 14:50:06 +0200

    add cassandra crt volume to delegation verify jobs

--- uptime_service_validation/coordinator/server.py
+++ uptime_service_validation/coordinator/server.py
@@ -171,10 +171,22 @@ def setUpValidatorPods(time_intervals, logging, worker_image, worker_tag):
             ),  # 0777 permission in octal as int
         )
 
+        cassandra_ssl_volume = client.V1Volume(
+            name="cassandra-crt",
+            secret=client.V1SecretVolumeSource(
+                secret_name="uptime-service-cassandra-crt"
+            ),
+        )
+
         # Define the volumeMounts
         auth_volume_mount = client.V1VolumeMount(
             name="auth-volume",
             mount_path=os.environ.get("AUTH_VOLUME_MOUNT_PATH"),
+        )
+
+        cassandra_ssl_volume_mount = client.V1VolumeMount(
+            name="cassandra-crt",
+            mount_path="/certs",
         )
 
         entrypoint_volume_mount = client.V1VolumeMount(
@@ -198,7 +210,7 @@ def setUpValidatorPods(time_intervals, logging, worker_image, worker_tag):
             resources=resource_requirements_container,
             env=env_vars,
             image_pull_policy=os.environ.get("IMAGE_PULL_POLICY", "IfNotPresent"),
-            volume_mounts=[auth_volume_mount, entrypoint_volume_mount],
+            volume_mounts=[auth_volume_mount, entrypoint_volume_mount, cassandra_ssl_volume_mount],
         )
 
         # Define the init container
@@ -224,7 +236,7 @@ def setUpValidatorPods(time_intervals, logging, worker_image, worker_tag):
                         containers=[container],
                         restart_policy="Never",
                         service_account_name=service_account_name,
-                        volumes=[auth_volume, entrypoint_volume],
+                        volumes=[auth_volume, entrypoint_volume, cassandra_ssl_volume],
                     )
                 ),
             ),
