26f97429fdee25820744e60ff96b398dac143493
Piotr Stachyra <piotr.stachyra@gmail.com>
2024-04-02 14:49:44 +0200

    filter_state_hash_percentage = 0.05

--- uptime_service_validation/coordinator/helper.py
+++ uptime_service_validation/coordinator/helper.py
@@ -314,18 +314,18 @@ def find_new_values_to_insert(existing_values, new_values):
     )
 
 
-def filter_state_hash_percentage(df, p=0.34):
+def filter_state_hash_percentage(df, p=0.05):
     "Filter statehashes by percentage of block producers who submitted them."
     state_hash_list = (
         df["state_hash"].value_counts().sort_values(ascending=False).index.to_list()
     )
-    # get 34% number of blk in given batch
+    # get 5% number of blk in given batch
     total_unique_blk = df["block_producer_key"].nunique()
     percentage_result = round(total_unique_blk * p, 2)
     good_state_hash_list = list()
     for s in state_hash_list:
         blk_count = df[df["state_hash"] == s]["block_producer_key"].nunique()
-        # check blk_count for state_hash submitted by blk least 34%
+        # check blk_count for state_hash submitted by blk least 5%
         if blk_count >= percentage_result:
             good_state_hash_list.append(s)
     return good_state_hash_list
